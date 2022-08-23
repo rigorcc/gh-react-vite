b51c0997f19c8423ac528d3b227b703751e3dc62
RigoRC <rcarvajalc3@gmail.com>
2022-08-23 12:26:29 -0400

    Fix 4to  bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://rigorcc.github.io/',
+  base:'https://rigorcc.github.io/gh-react-vite',
   plugins: [react()]
 })
