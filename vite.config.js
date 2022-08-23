053c9fce56e66315e72c4e784c922fed4b4c35a8
RigoRC <rcarvajalc3@gmail.com>
2022-08-23 11:41:41 -0400

    Fix 2do  bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'http://https://rigorcc.github.io/',
+  base:'https://https://rigorcc.github.io/',
   plugins: [react()]
 })
